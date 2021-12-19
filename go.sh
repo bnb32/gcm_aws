c3325de00308ad89002c76a1fcca73ecc5824fe4
Brandon N Benton <b.benton19@gmail.com>
2021-12-19 06:50:25 -0800

    Create go.sh

---
+++ go.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Mounting storage"
+
+bash ./scripts/storage.sh
+
+echo "Downloading cesm"
+
+python ./scripts/init_cesm.py
+
+echo "Downloading and installing isca"
+
+python ./scripts/init_isca.py
+
+echo "Downloading paleo-continent maps"
+
+python ./scripts/get_maps.py -download
+
+echo "Installing packages"
+
+python ./scripts/init_env.py
